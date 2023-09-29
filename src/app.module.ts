6938405adcad69ea98412bdb5cb12033f95fe61e
Brett Gibson <brettgibson5@gmail.com>
2023-09-29 00:35:50 -0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { EventModule } from './event/event.module';
+
+@Module({
+  imports: [AuthModule, UserModule, EventModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
