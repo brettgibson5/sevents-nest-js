ef74b2680464fc9d383370789935cb7879a063f2
Brett Gibson <brettgibson5@gmail.com>
2023-11-05 20:57:35 -0600

    guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { EventModule } from './event/event.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, EventModule, PrismaModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    EventModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
